519cd91b37df25ffe7d5a06917b80f5f4f13f842
Edward Kmett <ekmett@gmail.com>
2016-01-17 15:20:53 -0500

    Give back error "Not implemented" rather than failed pattern matches for GADT constructors on template-haskell 2.11

--- src/Bound/TH.hs
+++ src/Bound/TH.hs
@@ -15,7 +15,7 @@
 -- 'Monad' instances for data types.
 ----------------------------------------------------------------------------
 
-module Bound.TH 
+module Bound.TH
   (
 #ifdef MIN_VERSION_template_haskell
     makeBound
@@ -32,66 +32,66 @@ import Language.Haskell.TH
 import Control.Applicative (Applicative, pure, (<*>))
 #endif
 
--- | 
+-- |
 -- Use to automatically derive 'Applicative' and 'Monad' instances for
 -- your datatype.
--- 
+--
 -- In GHC 7.10 or later the @DeriveAnyClass@ extension may be used to derive the 'Show1' and 'Read1' instances
--- 
+--
 -- @
 -- {-# LANGUAGE DeriveAnyClass  #-}
 -- {-# LANGUAGE DeriveFunctor   #-}
 -- {-# LANGUAGE TemplateHaskell #-}
--- 
+--
 -- import Bound          (Scope, makeBound)
 -- import Prelude.Extras (Read1, Show1)
 --
--- data Exp a 
---   = V a 
---   | App (Exp a) (Exp a) 
---   | Lam (Scope () Exp a) 
---   | I Int 
+-- data Exp a
+--   = V a
+--   | App (Exp a) (Exp a)
+--   | Lam (Scope () Exp a)
+--   | I Int
 --   deriving (Functor, Read, Read1, Show, Show1)
--- 
+--
 -- makeBound ''Exp
 -- @
--- 
+--
 -- and in GHCi
--- 
+--
 -- @
 -- ghci> :set -XDeriveAnyClass
--- ghci> :set -XDeriveFunctor 
--- ghci> :set -XTemplateHaskell 
+-- ghci> :set -XDeriveFunctor
+-- ghci> :set -XTemplateHaskell
 -- ghci> import Bound          (Scope, makeBound)
 -- ghci> import Prelude.Extras (Read1, Show1)
 -- ghci> data Exp a = V a | App (Exp a) (Exp a) | Lam (Scope () Exp a) | I Int deriving (Functor, Read, Read1, Show, Show1); makeBound ''Exp
 -- @
 --
 -- or
--- 
--- @ 
+--
+-- @
 -- ghci> :{
 -- ghci| data Exp a = V a | App (Exp a) (Exp a) | Lam (Scope () Exp a) | I Int deriving (Functor, Read, Read1, Show, Show1)
 -- ghci| makeBound ''Exp
 -- ghci| :}
--- 
+--
 -- If @DeriveAnyClass@ is not used the instances must be declared explicitly:
--- 
+--
 -- @
--- data Exp a 
---   = V a 
---   | App (Exp a) (Exp a) 
---   | Lam (Scope () Exp a) 
---   | I Int 
+-- data Exp a
+--   = V a
+--   | App (Exp a) (Exp a)
+--   | Lam (Scope () Exp a)
+--   | I Int
 --   deriving (Functor, Read, Show)
 -- instance Read1 Exp
 -- instance Show1 Exp
--- 
+--
 -- makeBound ''Exp
--- @ 
--- 
+-- @
+--
 -- or in GHCi:
--- 
+--
 -- @
 -- ghci> :{
 -- ghci| data Exp a = V a | App (Exp a) (Exp a) | Lam (Scope () Exp a) | I Int deriving (Functor, Read, Show)
@@ -100,36 +100,36 @@ import Control.Applicative (Applicative, pure, (<*>))
 -- ghci| makeBound ''Exp
 -- ghci| :}
 -- @
--- 
+--
 -- 'Eq' and 'Ord' instances need to be derived differently if the data
 -- type's immediate components include 'Scope' (or other instances of
 -- 'Bound')
--- 
+--
 -- In a file with @{-# LANGUAGE StandaloneDeriving #-}@ at the top:
--- 
+--
 -- @
 -- instance Eq1 Exp
 -- deriving instance Eq a => Eq (Exp a)
--- 
+--
 -- instance Ord1 Exp
 -- deriving instance Ord a => Ord (Exp a)
 -- @
--- 
+--
 -- or in GHCi:
 --
 -- @
--- ghci> :set -XStandaloneDeriving 
+-- ghci> :set -XStandaloneDeriving
 -- ghci> deriving instance Eq a => Eq (Exp a); instance Eq1 Exp
 -- ghci> deriving instance Ord a => Ord (Exp a); instance Ord1 Exp
 -- @
--- 
+--
 -- because their 'Eq' and 'Ord' instances require @Exp@ to be a 'Monad':
--- 
+--
 -- @
 --   instance (Monad f, Eq b, Eq1 f, Eq a)    => Eq (Scope b f a)
 --   instance (Monad f, Ord b, Ord1 f, Ord a) => Ord (Scope b f a)
 -- @
--- 
+--
 -- Does not work yet for components that are lists or instances of
 -- 'Functor' or with a great deal other things.
 
@@ -145,24 +145,24 @@ makeBound name = do
       def :: Name -> DecQ -> [DecQ]
 #if __GLASGOW_HASKELL__ < 706
       def _theName dec = [dec]
-#else 
+#else
       def theName  dec = [pragInlD theName Inline FunLike AllPhases, dec]
 #endif
 
       pureBody :: Name -> [DecQ]
-      pureBody pure'or'return = 
-        def pure'or'return 
+      pureBody pure'or'return =
+        def pure'or'return
           (valD (varP pure'or'return) (normalB var) [])
 
       bindBody :: [DecQ]
-      bindBody = 
+      bindBody =
         def '(>>=)
           (valD (varP '(>>=)) (normalB bind) [])
 
-  apBody <- do 
-    ff <- newName "ff"  
+  apBody <- do
+    ff <- newName "ff"
     fy <- newName "fy"
-    f  <- newName "f"  
+    f  <- newName "f"
     y  <- newName "y"
 
     -- \ff fy -> do
@@ -170,7 +170,7 @@ makeBound name = do
     --   y <- fy
     --   pure (f x)
     let ap :: ExpQ
-        ap = lamE [varP ff, varP fy] (doE 
+        ap = lamE [varP ff, varP fy] (doE
               [bindS   (varP f) (varE ff),
                bindS   (varP y) (varE fy),
                noBindS (varE 'pure `appE` (varE f `appE` varE y))])
@@ -183,14 +183,14 @@ makeBound name = do
   --     f <- ff
   --     y <- fy
   --     pure (f y)
-  applicative <- 
+  applicative <-
     instanceD (cxt []) (appT (conT ''Applicative) (conT name))
       (pureBody 'pure ++ apBody)
 
   -- instance Monad $name where
   --   return = $var
   --   (>>=)  = $bind
-  monad <- 
+  monad <-
     instanceD (cxt []) (appT (conT ''Monad) (conT name))
       (pureBody 'return ++ bindBody)
 
@@ -218,13 +218,13 @@ makeBound name = do
 #endif
 
 -- Internals
-data Prop 
-  = Bound 
-  | Konst 
-  | Exp 
+data Prop
+  = Bound
+  | Konst
+  | Exp
   deriving Show
 
-data Components 
+data Components
   = Component Name [(Name, Prop)]
   | Variable Name
   deriving Show
@@ -244,8 +244,8 @@ construct (DataD _ name tyvar constructors _) = do
   var <- getPure name
   for constructors $ \con -> do
     case con of
-      NormalC conName [(_, _)] 
-        | conName == var 
+      NormalC conName [(_, _)]
+        | conName == var
         -> pure (Variable conName)
       NormalC conName types
         -> Component conName `fmap` mapM typeToBnd [ ty | (_, ty) <- types ]
@@ -256,7 +256,8 @@ construct (DataD _ name tyvar constructors _) = do
         bndA <- typeToBnd a
         bndB <- typeToBnd b
         pure (Component conName [bndA, bndB])
-      ForallC{} -> error "Not implemented."
+      _ -> error "Not implemented."
+
 
   where
   expa :: Type
@@ -266,12 +267,12 @@ construct (DataD _ name tyvar constructors _) = do
   typeToBnd ty = do
     boundInstance <- isBound ty
     var <- newName "var"
-    pure $ 
-      case () of () 
+    pure $
+      case () of ()
                    | ty == expa    -> (var, Exp)
                    | boundInstance -> (var, Bound)
                    | ConT{} <- ty  -> (var, Konst)
-                   | otherwise     -> error $ "This is bad: " 
+                   | otherwise     -> error $ "This is bad: "
                                            ++ show ty
                                            ++ " "
                                            ++ show boundInstance
@@ -282,7 +283,7 @@ construct (DataD _ name tyvar constructors _) = do
   --  -> isInstance ''Bound [Scope ()]
   --  -> True
   isBound :: Type -> Q Bool
-  isBound ty 
+  isBound ty
     | Just a <- stripLast2 ty = isInstance ''Bound [a]
     | otherwise               = return False
 construct _ = error "Must be a data type."
@@ -303,36 +304,36 @@ interpret bnds = do
 
     bind (Component name bounds) = do
      exprs <- foldM bindOne (ConE name) bounds
-     pure $ 
+     pure $
        Match
        (ConP name [ VarP arg | (arg, _) <- bounds ])
-       (NormalB 
+       (NormalB
          exprs)
         []
 
     bindOne :: Exp -> (Name, Prop) -> Q Exp
     bindOne expr (name, bnd) = case bnd of
-      Bound -> 
+      Bound ->
         pure expr `appE` (varE '(>>>=) `appE` varE name `appE` varE f)
-      Konst -> 
+      Konst ->
         pure expr `appE` varE name
-      Exp   -> 
+      Exp   ->
         pure expr `appE` (varE '(>>=) `appE` varE name `appE` varE f)
 
   matches <- for bnds bind
   pure $ LamE [VarP x, VarP f] (CaseE (VarE x) matches)
 
 stripLast2 :: Type -> Maybe Type
-stripLast2 (a `AppT` b `AppT` _ `AppT` d) 
+stripLast2 (a `AppT` b `AppT` _ `AppT` d)
   | AppT{} <- d = Nothing
   | otherwise   = Just (a `AppT` b)
-stripLast2 _ = Nothing 
+stripLast2 _ = Nothing
 
 getName :: TyVarBndr -> Name
 getName (PlainTV name)    = name
 getName (KindedTV name _) = name
 
--- Returns candidate 
+-- Returns candidate
 getPure :: Name -> Q Name
 getPure name = do
 #if MIN_VERSION_template_haskell(2,11,0)
@@ -341,9 +342,9 @@ getPure name = do
   TyConI (DataD _ _ tyvr cons _) <- reify name
 #endif
 
-  let 
+  let
     findReturn :: Type -> [(Name, [Type])] -> Name
-    findReturn ty constrs = 
+    findReturn ty constrs =
       case [ constr | (constr, [ty']) <- constrs, ty' == ty ] of
         []  -> error "Too few candidates for a variable constructor."
         [x] -> x
@@ -353,22 +354,23 @@ getPure name = do
         xs  -> error ("Too many candidates: " ++ intercalate ", " (map pprint xs))
 
     -- Gets the last type variable, given 'data Exp a b c = ...'
-    -- 
+    --
     --   lastTyVar = c
     lastTyVar :: Type
     lastTyVar = VarT (last (map getName tyvr))
 
     allTypeArgs :: Con -> (Name, [Type])
     allTypeArgs con = case con of
-      NormalC conName tys -> 
+      NormalC conName tys ->
         (conName, [ ty |    (_, ty) <- tys ])
-      RecC conName tys -> 
+      RecC conName tys ->
         (conName, [ ty | (_, _, ty) <- tys ])
-      InfixC (_, t1) conName (_, t2) -> 
+      InfixC (_, t1) conName (_, t2) ->
         (conName, [ t1, t2 ])
       ForallC _ _ conName ->
          allTypeArgs conName
+      _ -> error "Not implemented"
 
-  return (findReturn lastTyVar (allTypeArgs `fmap` cons)) 
+  return (findReturn lastTyVar (allTypeArgs `fmap` cons))
 #else
 #endif
