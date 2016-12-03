9212d9ed290e59a888fdd2f873c632c3d611c17d
Oleg Grenrus <oleg.grenrus@iki.fi>
2016-12-03 20:25:50 +0200

    Use deriving-compat in examples/Simple.hs

--- examples/Simple.hs
+++ examples/Simple.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE CPP #-}
+{-# LANGUAGE CPP, TemplateHaskell #-}
 module Main where
 
 -- this is a simple example where lambdas only bind a single variable at a time
@@ -11,9 +11,11 @@ import Data.Traversable
 import Control.Monad
 import Control.Applicative
 import Prelude hiding (foldr,abs)
+import Data.Deriving (deriveEq1, deriveOrd1, deriveRead1, deriveShow1)
 import Data.Functor.Classes
 import Bound
 import System.Exit
+
 
 infixl 9 :@
 
@@ -24,20 +26,10 @@ data Exp a
   | Let [Scope Int Exp a] (Scope Int Exp a)
   deriving (Eq)
 
-#if MIN_VERSION_transformers(0,5,0) || !MIN_VERSION_transformers(0,4,0)
-instance Eq1 Exp where
-  liftEq g (V a) (V b) = g a b
-  liftEq g (a :@ a') (b :@ b') = liftEq g a b && liftEq g a' b'
-  liftEq g (Lam a) (Lam b) = liftEq g a b
-  liftEq g (Let a a') (Let b b') =
-      liftEq (liftEq g) a b && liftEq g a' b'
-  liftEq _ _ _ = False
-
-#else
-
--- these 4 classes are needed to help Eq, Ord, Show and Read pass through Scope
-instance Eq1 Exp      where eq1        = (==)
-#endif
+deriveEq1   ''Exp
+deriveOrd1  ''Exp
+deriveRead1 ''Exp
+deriveShow1 ''Exp
 
 -- | A smart constructor for Lam
 --
@@ -72,39 +64,6 @@ instance Monad Exp where
   (x :@ y) >>= f = (x >>= f) :@ (y >>= f)
   Lam e    >>= f = Lam (e >>>= f)
   Let bs b >>= f = Let (map (>>>= f) bs) (b >>>= f)
-
--- these 4 classes are needed to help Eq, Ord, Show and Read pass through Scope
-#if (MIN_VERSION_transformers(0,5,0)) || !(MIN_VERSION_transformers(0,4,0))
-instance Eq1 Exp where
-  liftEq f (V a) (V b) = f a b
-  liftEq f (a :@ b) (c :@ d) = liftEq f a c && liftEq f b d
-  liftEq f (Lam m) (Lam n) = liftEq f m n 
-  liftEq f (Let xs b) (Let ys c) = liftEq (liftEq f) xs ys && liftEq f b c
-  liftEq _ _ _ = False
-
-instance Ord1 Exp where
-  liftCompare f (V a) (V b) = f a b
-  liftCompare _ V{} _ = LT
-  liftCompare _ _ V{} = GT
-  liftCompare f (a :@ b) (c :@ d) = liftCompare f a c `mappend` liftCompare f b d
-  liftCompare _ (_ :@ _) _  `= LT
-  liftCompare _ (Lam _) (_ :@ _) = GT
-  liftCompare f (Lam m) (Lam n) = liftCompare f m n 
-  liftCompare f (Let xs b) (Let ys c) = liftCompare (liftCompare f) xs ys `mappend` liftCompare f b c
-  liftCompare _ _ (Let _ _) = LT
-
--- ugh...
-instance Show1 Exp where
-  liftShowsPrec = error "I'm too lazy to evaluate this"
-
-instance Read1 Exp where
-  liftReadsPrec = error "I'm too lazy to evaluate this"
-#else
-instance Eq1 Exp      where eq1 = (==)
-instance Ord1 Exp     where compare1 = compare
-instance Show1 Exp    where showsPrec1 = showsPrec
-instance Read1 Exp    where readsPrec1 = readsPrec
-#endif
 
 -- | Compute the normal form of an expression
 nf :: Exp a -> Exp a
